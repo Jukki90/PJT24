7223fb26363b50d2824f3f21eeba29d0777d6ed5
Jukki90 <juliaylanova@mail.ru>
2015-06-25 20:41:12 +0300

    first lesson

---
+++ Java_selenium_lesson1/src/com/example/tests/GroupData.java
@@ -0,0 +1,17 @@
+package com.example.tests;
+
+public class GroupData {
+	public String name;
+	public String header;
+	public String footer;
+	
+	public GroupData() {
+		
+	}
+
+	public GroupData(String nameGrup, String header, String footer) {
+		this.name = nameGrup;
+		this.header = header;
+		this.footer = footer;
+	}
+}
